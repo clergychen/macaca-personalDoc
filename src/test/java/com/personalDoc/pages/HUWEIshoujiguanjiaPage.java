d7148882dc73fdead46b4952269773a6c081ee65
陈俊 <chenjun@hys-inc.com>
2017-06-06 09:56:33 +0800

    每日集成-0606

---
+++ src/test/java/com/personalDoc/pages/HUWEIshoujiguanjiaPage.java
@@ -0,0 +1,7 @@
+package com.personalDoc.pages;
+
+/**
+ * Created by chenjun on 2017/6/2.
+ */
+public class HUWEIshoujiguanjiaPage {
+}
---
+++ src/test/java/com/personalDoc/pageuis/HUWEIshoujiguanjiaPageUI.java
@@ -0,0 +1,7 @@
+package com.personalDoc.pageuis;
+
+/**
+ * Created by chenjun on 2017/6/2.
+ */
+public class HUWEIshoujiguanjiaPageUI {
+}
